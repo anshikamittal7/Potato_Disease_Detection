8838b80347489abe2a682069779cf48ba83fc187
anshikamittal7 <anshikamittal34@gmail.com>
2023-10-13 13:41:52 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import HomeScreen from "./screens/HomeScreen";
-
+import Header, { HeaderPhone } from "./components/Header";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return <BrowserRouter>
+    <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <Routes>
       <Route path="/" element={<HomeScreen />} />
     </Routes>
-    <Toaster/>
+    <Toaster />
   </BrowserRouter>
 }
 
